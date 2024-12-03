c32ec752b578572a1f7730317c66d680e0e67941
Viktor9125 <152290176+Viktor9125@users.noreply.github.com>
2024-12-03 16:30:56 +0300

    Add files via upload

---
+++ plant.py
@@ -0,0 +1,41 @@
+# class Plant:
+#     def __init__(self, name):
+#         self.name = name
+#
+#     def water_plant(self):
+#         ...
+
+# def create_plant():
+#     plant = {'name': 'Кактус',
+#              'water': 10,
+#              'fertilizer': 10,
+#              'sunlight': 10}
+#     return plant
+#
+# def water_plant(plant):
+#     plant['water'] += 2
+#     if plant['water'] > 10:
+#         plant['water'] = 10
+#
+# def fertilize_plant(plant):
+#     plant['fertilizer'] += 2
+#     if plant['fertilizer'] > 10:
+#         plant['fertilizer'] = 10
+#
+# def give_sunlight(plant):
+#     plant['sunlight'] += 2
+#     if plant['sunlight'] > 10:
+#         plant['sunlight'] = 10
+#
+#
+# def main():
+#     plant = create_plant()
+#     print(f"Ваше растение: {plant['name']}")
+#
+#     water_plant(plant)
+#     fertilize_plant(plant)
+#     give_sunlight(plant)
+#
+#     print(plant)
+#
+# main()
